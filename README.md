8cbd8ea7f74e467b8d8055cd29835e775454b9bc
Dan Shirley <danshirley@Dans-MacBook-Pro.local>
2016-12-28 18:53:40 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hmrc-practical-test
 A short practical test
+
+## Overview
+The requirements are specified in document:
+/docs/Front End Developer.docx
+
+
---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+/**
+ * Pure-vanilla JS, no jQuery.
+ * No ES6 in here as we're not pre-processing (hence the vars).
+ * @author Dan Shirley
+ */
+window.hmrcNS = function() {
+  var noJsClass = 'no-js';
+
+  /**
+   * Set help text to anchor element, which is revealed with JS.
+   */
+  var setNiNumberHelpText = function() {
+    var helpTextElement = document.getElementById('ni-number-help-text')
+    var anchorElement = document.getElementById("ni-number-help-prompt");
+
+    anchorElement.setAttribute('title', helpTextElement.textContent);
+    anchorElement.style.display = 'inline';
+  }
+
+  var hideNoJsContent = function() {
+    var noJsElements = document.getElementsByClassName(noJsClass);
+
+    for (var x = 0; x < noJsElements.length; x++) {
+      noJsElements[x].style.display = 'none';
+    }
+  }
+
+  return {
+    doJsEnabledActions: () => {
+      setNiNumberHelpText();
+      hideNoJsContent();
+    }
+  }
+}();
+
+// Run on DOM ready
+document.addEventListener('DOMContentLoaded', function(event) { 
+  hmrcNS.doJsEnabledActions();
+});
